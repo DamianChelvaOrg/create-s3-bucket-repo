882677f8f00e0b6b49d35536adee68e38b1e7026
Damian Chelverajan <53366344+DamianChelva@users.noreply.github.com>
2026-03-18 16:07:22 +1100

    Remove S3 bucket ACL resource definition

--- templates/cloudResource.tf
+++ templates/cloudResource.tf
@@ -1,8 +1,3 @@
 resource "aws_s3_bucket" "{{ bucket_name }}" {
   bucket = "{{ bucket_name }}"
 }
-
-resource "aws_s3_bucket_acl" "{{ bucket_name }}_acl" {
-  bucket = aws_s3_bucket.{{ bucket_name }}.id
-  acl    = "{{ bucket_acl }}"
-}
