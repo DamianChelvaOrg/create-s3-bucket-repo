17562d377e1b9e5765de9c2ddc06d360924384aa
Damian Chelverajan <53366344+DamianChelva@users.noreply.github.com>
2026-03-18 16:08:25 +1100

    Delete resources/jamesbondport007.tf

--- resources/jamesbondport007.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "jamesbondport007" {
-  bucket = "jamesbondport007"
-}
-
-resource "aws_s3_bucket_acl" "jamesbondport007_acl" {
-  bucket = aws_s3_bucket.jamesbondport007.id
-  acl    = "public-read"
-}
