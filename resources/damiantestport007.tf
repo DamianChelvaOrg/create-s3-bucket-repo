edfaa57267032beff0f3c5f6e11e4734d0128370
Damian Chelverajan <53366344+DamianChelva@users.noreply.github.com>
2026-03-18 15:48:07 +1100

    Update damiantestport007.tf

--- resources/damiantestport007.tf
+++ resources/damiantestport007.tf
@@ -4,5 +4,5 @@ resource "aws_s3_bucket" "damiantestport007" {
 
 resource "aws_s3_bucket_acl" "damiantestport007_acl" {
   bucket = aws_s3_bucket.damiantestport007.id
-  acl    = "public"
+  acl    = "public-read"
 }
