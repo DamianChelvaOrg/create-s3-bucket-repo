cef957b02dbce711a91f50fa6df884d2f5a4bb73
Damian Chelverajan <53366344+DamianChelva@users.noreply.github.com>
2026-03-18 15:51:40 +1100

    Delete S3 bucket ACL resource
    Removed the AWS S3 bucket ACL resource for damiantestport007.

--- resources/damiantestport007.tf
+++ resources/damiantestport007.tf
@@ -1,8 +1,3 @@
 resource "aws_s3_bucket" "damiantestport007" {
   bucket = "damiantestport007"
 }
-
-resource "aws_s3_bucket_acl" "damiantestport007_acl" {
-  bucket = aws_s3_bucket.damiantestport007.id
-  acl    = "public-read"
-}
